2812af700b5b5f0ec730ba84ae8c767cb1b78d0d
Pasha <korovinlive@gmail.com>
2026-01-29 22:41:14 +0300

    new func load_notes, save_notes

---
+++ core/config.py
@@ -0,0 +1,9 @@
+STRUCTURE_NOTE = {
+            "id": "",
+            "text": "",
+            "tags": [],
+            "created": "",
+            "updated": ""
+                }
+
+FILE_PATH = r"data\test_data.json"
--- core/notes_manager.py
+++ core/notes_manager.py
@@ -1,19 +1,43 @@
-
+import json
+import os
+import datetime
+from config import STRUCTURE_NOTE, FILE_PATH
 
 class NotesManager():
     
-    def load_notes(self, filename) -> list[dict]:
-        with open(filename, 'r') as f:
-            file = f.readline()
-            print(file)
-        return file
-        
+    def __init__(self):
+        self.FILE_PATH = FILE_PATH
     
-    def save_notes(self, notes, filename):
-        pass
+    def load_notes(self):
+        #тут проверяем на наличие файла, если его нет, то создаём 
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w', encoding='utf8') as f:
+                json.dump([], f)
+            return []
+        #пытаемся ошибку на декодирование json a 
+        try:
+            with open(self.filename, 'r', encoding='utf8') as f:
+                return json.load(f)
+        except json.JSONDecodeError:
+            print(f'Что-то не так с файлом {self.filename}')
+            
+    def save_notes(self, notes):
+        #Полная перезапись файла
+        if notes:
+            with open(self.filename, 'w', encoding='utf8') as f:
+                json.dump(notes, f)
+            return f'Данные записаны в {self.filename}'
+        else:
+            return 'Добавьте данные что бы сохранить'
     
-    def add_note(self, text, tags):
-        pass
+    def add_note(self, text: str, tags: list):
+        #Распаршиваем заметку в структуру json
+        new_note = STRUCTURE_NOTE
+        new_note['id'] = ''
+        new_note['text'] = text
+        new_note['tags'] = tags
+        new_note['created'] = str(datetime.now())
+        print(new_note)
     
     def update_note(self, note_id, text, tags):
         pass
@@ -27,4 +51,10 @@ class NotesManager():
     def generate_id():
         pass
     
-x = NotesManager
+    # def __str__(self):
+    #     print(self.filename)
+    
+
+x = NotesManager()
+
+r = x.load_notes()
