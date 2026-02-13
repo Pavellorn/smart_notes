f60c632b5c89f94c4dde954a5615f368509db034
Pavel <pashasokolov779@gmail.com>
2026-02-13 22:08:50 +0300

    Изменил метод поиска и генерацию id. Переписал тесты для реального файла. Добавил .gitignore

--- test_app_notes.py
+++ test_app_notes.py
@@ -3,46 +3,38 @@ from notes_manager import NotesManager
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
-file_path = BASE_DIR / "data.json"
-
-
-
+file_path = BASE_DIR / "notes.json"
 
 
 
 def test_reading_real_file():
-    """
-    Здесь проверяю основной функционал класса NotesManager:
-    - добавление заметки
-    - чтение заметки из файла
-    - сохранение изменений в файле
-    - удаление заметок
-    - искать по фрагментам текста заметки и теги
-    """
     note_1 = NotesManager()
-    note_1.filename = file_path  # Передаю реальный файл
+    note_1.filename = file_path
 
     note_1.load_notes()
-    assert len(note_1.notes) == 3  # в моём реальном файле 3 заметки
+    assert len(note_1.notes) == 2
 
     # Добавляю тестовую заметку
-    note_1.add_note("Тестовая заметка №1", "#test1")
-    assert len(note_1.notes) > 3
-
-    result = note_1.search_notes("Идеи")
-    assert len(result) == 1
-    assert result[0]["id"] == 2
-    assert "Идеи для улучшения интерфейса" in result[0]["text"]
-
-    result2 = note_1.search_notes("интерфейс")
-    assert "интерфейс" in result2[0]["tags"]
-
-    # проверяю, что удалится запись 4, которую я добавлю в файл
-    note_1.delete_note(4)
+    note_1.add_note("Тестовая заметка №2", "#test1")
     assert len(note_1.notes) == 3
-    assert (
-        note_1.notes[2]["id"] != 4
-    )  # здесь я проверяю, что третья записать осталась, а 4 удалилась, и id = 4 нет
+
+    # Поиск по квант в тексте Квантовый
+    result = note_1.search_notes("квант")
+    assert len(result) == 1
+    assert result[0]["id"] == 1
+    assert "квант" in result[0]["text"].lower()
+
+    # Поиск по "зам"
+    result2 = note_1.search_notes("зам")
+    assert len(result2) >= 1
+    assert "test1" in str(result2[-1]["tags"])
+
+    # Удаление заметки
+    note_1.delete_note(2)
+    assert len(note_1.notes) == 2
+
+    ids = [note["id"] for note in note_1.notes]
+    assert 2 not in ids
 
 
 
